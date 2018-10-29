0c48189281a0a2d325ecd08ad59d961de66440f6
DigitalCoinBRL <sperocoin@gmail.com>
2018-10-29 07:49:40 -0300

    Informação 'No More PoW Blocks' repassada para o minerador

--- src/rpcmining.cpp
+++ src/rpcmining.cpp
@@ -123,8 +123,8 @@ Value getworkex(const Array& params, bool fHelp)
     if (IsInitialBlockDownload())
         throw JSONRPCError(-10, "SperoCoin is downloading blocks...");
 
-    //if (pindexBest->nHeight >= LAST_POW_BLOCK)
-    //    throw JSONRPCError(RPC_MISC_ERROR, "No more PoW blocks");
+    if (pindexBest->nHeight >= LAST_POW_BLOCK)
+        throw JSONRPCError(RPC_MISC_ERROR, "No more PoW blocks");
 
     typedef map<uint256, pair<CBlock*, CScript> > mapNewBlock_t;
     static mapNewBlock_t mapNewBlock;
@@ -256,8 +256,8 @@ Value getwork(const Array& params, bool fHelp)
     if (IsInitialBlockDownload())
         throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "SperoCoin is downloading blocks...");
 
-    //if (pindexBest->nHeight >= LAST_POW_BLOCK)
-    //    throw JSONRPCError(RPC_MISC_ERROR, "No more PoW blocks");
+    if (pindexBest->nHeight >= LAST_POW_BLOCK)
+        throw JSONRPCError(RPC_MISC_ERROR, "No more PoW blocks");
 
     typedef map<uint256, pair<CBlock*, CScript> > mapNewBlock_t;
     static mapNewBlock_t mapNewBlock;    // FIXME: thread safety
@@ -400,8 +400,8 @@ Value getblocktemplate(const Array& params, bool fHelp)
     if (IsInitialBlockDownload())
         throw JSONRPCError(RPC_CLIENT_IN_INITIAL_DOWNLOAD, "SperoCoin is downloading blocks...");
 
-    //if (pindexBest->nHeight >= LAST_POW_BLOCK)
-    //    throw JSONRPCError(RPC_MISC_ERROR, "No more PoW blocks");
+    if (pindexBest->nHeight >= LAST_POW_BLOCK)
+        throw JSONRPCError(RPC_MISC_ERROR, "No more PoW blocks");
 
     static CReserveKey reservekey(pwalletMain);
 
