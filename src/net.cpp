2aaf7488cb6d5bd3e2cfcfa663200163c9838e2c
SperoCoin <helper@sperocoin.org>
2019-05-30 21:04:21 -0300

    Update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -1168,10 +1168,10 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"sperocoin.ddns.net", "sperocoin.ddns.net"},
     {"seed1.sperocoin.org", "seed1.sperocoin.org"},
     {"seed2.sperocoin.org", "seed2.sperocoin.org"},
     {"seed3.sperocoin.org", "seed3.sperocoin.org"},
+    {"seed4.sperocoin.org", "seed4.sperocoin.org"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
