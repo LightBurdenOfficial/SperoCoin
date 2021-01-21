e6d29d3d1544b4ed144e7638e0049ca479d3f3ec
DigitalCoinBRL <32315718+DigitalCoin1@users.noreply.github.com>
2021-01-21 17:32:11 -0300

    [FIX] Endereços Foundation_POS

--- src/main.h
+++ src/main.h
@@ -69,6 +69,11 @@ static const int64_t COIN_POS_NEW_REWARD_02 = 0.25 * COIN; // 0.25 moedas por pa
 #define FOUNDATION_TEST "sbEje8AqUUqkUXXNGYJodR2SN3emT6PVzA"
 /* Fim Adaptação para pagamentos Foundation */
 
+/* Início Adaptação para pagamentos Foundation PoS */
+#define FOUNDATION_POS "ScCJWtVVQxtEkKDqnL43aJpzULvpEE5fBD"
+#define FOUNDATION_TEST_POS "saFFVBM6CGw7RTJLHdDJcizQ1V77XbkzTv"
+/* Fim Adaptação para pagamentos Foundation PoS */
+
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 // Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
 static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
