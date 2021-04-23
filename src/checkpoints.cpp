e2a6458986002ee876ad0db71ee5f3bc029ade8a
DigitalCoinBRL <32315718+DigitalCoin1@users.noreply.github.com>
2021-04-23 13:49:02 -0300

    [FIX] Hash 1150000

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -90,7 +90,7 @@ namespace Checkpoints
         ( 920000,      uint256("0x87c20a7e4900d4f754cc56e7f41320df30eee8f64696406a0e6632b1761f37d7") )
         ( 950000,      uint256("000006c886de34c565ee0b529dade796428862ad60c0d58478c60be29cb5faff") )
         ( 1000000,      uint256("000000a662fdd08ff4f246820ed41f23d56b595193c64380618ddbb2be6feb1b") )
-        ( 1150000,      uint256("d7acbe8977bf29980a5bf8522716a4b7ffac53c5e9c6c439c5878ebd19289817") ) //Halving PoW and PoS
+        ( 1150000,      uint256("000005a7f4b5c150f073e91b3666c859e85610e67bd0b09cb0284fbcf587179e") ) //Halving PoW and PoS
         
     ;
 
