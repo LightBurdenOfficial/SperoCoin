bb8008bd323a0de95290b9a2c8a34f84e5b27a1b
DigitalCoin1 <helper@sperocoin.org>
2021-12-16 16:10:19 +0000

    Ativação Segwit

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -1750,12 +1750,10 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
         }
 
         int minVersion = MIN_PEER_PROTO_VERSION;
-        /* START FRANCIS MOD
         // SperoCoin: The code base will fork two weeks before segwit activates, until that point we will allow older versions to connect.
         if((chainparams.GetConsensus().vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime - 1209600) > GetAdjustedTime()) {
-            minVersion = GETHEADERS_VERSION; // The previous MIN_PEER_PROTO_VERSION.
+            minVersion = SEGWIT_VERSION; // The previous MIN_PEER_PROTO_VERSION.
         }
-        END FRANCIS MOD */
 
         if (nVersion < minVersion)
         {
